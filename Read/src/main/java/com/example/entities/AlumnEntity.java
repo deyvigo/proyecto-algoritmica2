19347271eecf2cb800162b3e36293f75f56dd476
Adrian Palacios <78890986+AdrianP17@users.noreply.github.com>
2023-11-18 16:19:22 -0500

    Update AlumnEntity.java

--- Read/src/main/java/com/example/entities/AlumnEntity.java
+++ Read/src/main/java/com/example/entities/AlumnEntity.java
@@ -28,4 +28,46 @@ public class AlumnEntity extends UserEntity{
     @OneToMany(mappedBy = "alumnSolve")
     private List<SolveEntity> solutions;
 
+    //Obtener promedio
+    public double calcularPromedio() {
+        if (solutions.isEmpty()) {
+            return 0.0;
+        }
+
+        double sumaNotas = solutions.stream().mapToDouble(SolveEntity::getNota).sum();
+        return sumaNotas / solutions.size();
+    }
+    public double calcularPromedioPreguntasCorrectasPorTexto() {
+        int totalPreguntas = solutions.stream()
+                .mapToInt(solve -> solve.getCorrects() + solve.getWrongs())
+                .sum();
+        
+        int totalCorrectas = solutions.stream()
+                .mapToInt(SolveEntity::getCorrects)
+                .sum();
+
+        return totalPreguntas > 0 ? ((double) totalCorrectas / totalPreguntas) * 100 : 0;
+    }
+
+    public double calcularPorcentajeTextosAcertadosCompletamente() {
+        if (solutions.isEmpty()) {
+            return 0.0;
+        }
+        long textosAcertadosCompletamente = solutions.stream()
+                .filter(solve -> solve.getNota() == 20)
+                .count();
+
+        return (double) textosAcertadosCompletamente / solutions.size() * 100;
+    }
+
+    public double calcularPorcentajeTextosFalladosCompletamente() {
+        if (solutions.isEmpty()) {
+            return 0.0;
+        }
+        long textosFalladosCompletamente = solutions.stream()
+                .filter(solve -> solve.getNota() == 0)
+                .count();
+
+        return (double) textosFalladosCompletamente / solutions.size() * 100;
+    }
 }
