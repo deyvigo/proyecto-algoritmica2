08bcdb24abce8b2402b52a834d46cda5055397c0
Adrian Palacios <78890986+AdrianP17@users.noreply.github.com>
2023-11-26 21:05:42 -0500

    Update AlumnRepository.java

--- Read/src/main/java/com/example/repositories/AlumnRepository.java
+++ Read/src/main/java/com/example/repositories/AlumnRepository.java
@@ -2,11 +2,17 @@ package com.example.repositories;
 
 import com.example.entities.AlumnEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface AlumnRepository extends JpaRepository<AlumnEntity, Long> {
     Optional<AlumnEntity> findByUsername(String username);
+
+    @Query("SELECT a FROM AlumnEntity a WHERE a.alumn_group.id = :groupId")
+    List<AlumnEntity> getAlumnsPerGroup(@Param("groupId") Long groupId);    
 }
