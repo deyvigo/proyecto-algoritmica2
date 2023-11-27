f13b1d9fb88d0dc7e2ea1d5b85bce53f4ea77bb1
Adrian Palacios <78890986+AdrianP17@users.noreply.github.com>
2023-11-26 21:06:22 -0500

    Update SolveRepository.java

--- Read/src/main/java/com/example/repositories/SolveRepository.java
+++ Read/src/main/java/com/example/repositories/SolveRepository.java
@@ -2,8 +2,12 @@ package com.example.repositories;
 
 import com.example.entities.SolveEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface SolveRepository extends JpaRepository<SolveEntity, Long> {
+    @Query("SELECT COUNT(s) FROM SolveEntity s WHERE s.alumnSolve.id = :alumnId")
+    int countSolvesByAlumnId(@Param("alumnId") Long alumnId);
 }
